0984b5fc97ad452221a3c396fcf89728b9306a08
ayoub <fadadi.ayoub@gmail.com>
2019-03-05 17:13:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card/Card';
+import DrawButton from './DrawButton/DrawButton';
+import { DB_CONFIG } from './Config/Firebase/db_config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('cards2');
+    this.updateCard = this.updateCard.bind(this);
+
+    this.state = {
+      cards: [],
+      currentCard: {}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards = this.state.cards;
+
+    this.database.on('child_added', snap => {
+      currentCards.push({
+        id: snap.key,
+        eng: snap.val().en,
+        han: snap.val().en,
+        fr: snap.val().fr
+      })
+
+      this.setState({
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards)
+      })
+    })
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    return card;
+  }
+
+  updateCard(){
+    const currentCards = this.state.cards;
+    console.log(currentCards);
+    this.setState({
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="cardRow">
+          <Card 
+            eng={this.state.currentCard.eng} 
+            han={this.state.currentCard.han} 
+            fr={this.state.currentCard.fr}
+          />
+        </div>
+        <div className="buttonRow">
+          <DrawButton drawCard={this.updateCard}/>
+        </div>
       </div>
     );
   }
---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAqncyntE_74GKLGboITJks1QkaAQ3fVKg",
+    authDomain: "react-flashcards-4f46e.firebaseapp.com",
+    databaseURL: "https://react-flashcards-4f46e.firebaseio.com",
+    projectId: "react-flashcards-4f46e",
+    storageBucket: "react-flashcards-4f46e.appspot.com",
+    messagingSenderId: "749017558697"
+  };
